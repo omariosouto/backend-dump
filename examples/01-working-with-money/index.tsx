718014b25f1198bbde10e9b0b241d10418ba48e5
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-03-22 13:16:49 -0300

    update

--- examples/01-working-with-money/index.tsx
+++ examples/01-working-with-money/index.tsx
@@ -5,7 +5,8 @@ console.log("[Working with money]");
 var BRL = value => currency(value, { symbol: "R$", separator: ".", decimal: ",", fromCents: true });
 
 
-const ISS = 2.00; // 2%
+// const ISS = 2.1; // 2.1%
+const ISS = 2.1111; // 2.1111%%
 // const ISS = 2; // 2%
 // const ISS = "2.10"; // 2%
 // const ISS = "10"; // 10%
@@ -17,17 +18,7 @@ const money = BRL(AMOUNT_IN_CENTS);
 
 console.log(`Amount: ${money.toString()}`);
 console.log(`${ISS}% of ${money.format()} is: ${money.multiply(toPercent(ISS)).toString()}`);
-
-
-
-
-
-
-
-
-
-
-
+console.log(`${ISS}% of ${AMOUNT_IN_CENTS} is: ${Number(BigInt(AMOUNT_IN_CENTS)) * toPercent(ISS)}`);
 
 
 
