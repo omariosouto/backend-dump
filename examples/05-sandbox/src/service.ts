26976b9c668d1fc6c62014ab2c7c0f8c8942cf73
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-05-13 14:49:27 -0300

    update

--- examples/05-sandbox/src/service.ts
+++ examples/05-sandbox/src/service.ts
@@ -1,8 +1,28 @@
 import express from "express";
-
+import bodyParser from "body-parser";
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+  const db = new PrismaClient();
+  (async () => {
+    const totalInsertions = await db.post.count();
+    console.log(totalInsertions);
+  })();
   res.send("Hello World");
+});
+
+app.post("/api/mutation", (req, res) => {
+  console.log("hit", req.body);
+  const body = req.body;
+  res.send(body);
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
